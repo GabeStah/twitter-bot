5b3c4ad16431cd0d68cac545561bad16f8e1fc64
Gabe Wyatt <gabe@gabewyatt.com>
2018-01-01 20:18:24 -0800

    Finalized AWS integration.

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const Twitter = require('twitter');
 // Use exported secret credentials.
 let airbrake = new AirbrakeClient(airbrake_credentials);
 
-let feedparser = new FeedParser();
-let feed = request('https://airbrake.io/blog/feed/atom');
-
 // Use exported secret credentials.
 let twitter = new Twitter(twitter_credentials);
 
@@ -19,52 +16,50 @@ let twitter = new Twitter(twitter_credentials);
 let articles = [];
 
 /**
- * Fires when feed request receives a response from server.
+ * Gets a random Article from collection.
+ *
+ * @returns {*} Randomly-chosen article.
  */
-feed.on('response', function (response) {
-    if (response.statusCode !== 200) {
-        this.emit('error', new Error('Bad status code.'));
-    } else {
-        // Pipes request to feedparser for processing.
-        this.pipe(feedparser);
-    }
-});
-
-/**
- * Invoked when feedparser completes processing request.
- */
-feedparser.on('end', function () {
-    tweetRandomArticle(articles);
-});
-
-/**
- * Executes when feedparser contains readable stream data.
- */
-feedparser.on('readable', function () {
-    let article;
-
-    // Iterate through all items in stream.
-    while (article = this.read()) {
-        // Output each Article to console.
-        console.log(`Gathered '${article.title}' published ${article.date}`);
-        // Add Article to collection.
-        articles.push(article);
-    }
-});
+function getRandomArticle() {
+    return articles[Math.floor(Math.random()*articles.length)];
+}
 
 /**
  * Tweet the passed string message.
  *
  * @param message String to be tweeted.
+ * @param succeed Success callback function.
+ * @param fail Failure callback function.
  */
-function tweet(message) {
-    if (message === null || message === '') return;
+function tweet(message, succeed, fail) {
+    if (message === null || message === '') {
+        fail('Blank message cannot be tweeted.');
+        return;
+    }
     twitter.post(
         'statuses/update',
         {
             status: message
         },
-        tweetCallback
+        // Callback function after response or failure.
+        function (error, tweet, response) {
+            if (error) {
+                // If error, output to log.
+                console.log(error);
+                // Report error to Airbrake.
+                airbrake.notify(error[0]).then(() => {
+                    // Return fail promise, to inform Lambda of failure.
+                    return fail(error[0].message);
+                });
+            } else {
+                // If success, output to log.
+                console.log('---- TWEETED ----');
+                console.log(tweet);
+                // Inform Lambda of success.
+                succeed(`'${tweet.text}' tweeted.`);
+            }
+        // Bind passed succeed/fail parameters to callback function params.
+        }.bind( {succeed: succeed, fail: fail} )
     );
 }
 
@@ -72,38 +67,78 @@ function tweet(message) {
  * Tweet the passed Article object.
  *
  * @param article Article to be tweeted.
+ * @param succeed Success callback function.
+ * @param fail Failure callback function.
  */
-function tweetArticle(article) {
+function tweetArticle(article, succeed, fail) {
     if (article === null) return;
-    twitter.post(
-        'statuses/update',
-        {
-            status: `${article.title} ${article.link}`
-        },
-        tweetCallback
-    );
+    // Invoke base tweet function.
+    tweet(`${article.title} ${article.link}`, succeed, fail);
 }
 
 /**
- * Callback for tweet attempts.
+ * Tweets Hello world! via AWS Lambda.
  *
- * @param error Caught error.
- * @param tweet Tweet.
- * @param response Response.
+ * @param event AWS Lambda event data for the handler.
+ * @param context AWS Lambda runtime information.
  */
-function tweetCallback(error, tweet, response) {
-    if(error) {
-        console.log(error);
-        throw new Error(error[0].message);
-    }
-    console.log('---- TWEETED ----');
-    console.log(tweet);
-}
+exports.tweetHelloWorld = function(event, context) {
+    tweet('Hello World', context.succeed, context.fail);
+};
 
 /**
- * Tweet a random Article.
+ * Tweets a random Airbrake article via AWS Lambda.
+ *
+ * @param event AWS Lambda event data for the handler.
+ * @param context AWS Lambda runtime information.
  */
-function tweetRandomArticle() {
-    // Tweet a random article.
-    tweetArticle(articles[Math.floor(Math.random()*articles.length)])
-}
+exports.tweetRandomArticle = function(event, context) {
+    let feedparser = new FeedParser();
+    let feed = request('https://airbrake.io/blog/feed/atom');
+
+    /**
+     * Fires when feed request receives a response from server.
+     */
+    feed.on('response', function (response) {
+        if (response.statusCode !== 200) {
+            let error = new Error('Bad status code.');
+            this.emit('error', error);
+        } else {
+            // Pipes request to feedparser for processing.
+            this.pipe(feedparser);
+        }
+    });
+
+    /**
+     * Invoked when feedparser completes processing request.
+     */
+    feedparser.on('end', function () {
+        tweetArticle(getRandomArticle(), context.succeed, context.fail);
+    });
+
+    /**
+     * Executes when feedparser contains readable stream data.
+     */
+    feedparser.on('readable', function () {
+        let article;
+
+        // Iterate through all items in stream.
+        while (article = this.read()) {
+            // Output each Article to console.
+            console.log(`Gathered '${article.title}' published ${article.date}`);
+            // Add Article to collection.
+            articles.push(article);
+        }
+    });
+};
+
+/**
+ * Tweets the current time via AWS Lambda.
+ *
+ * @param event AWS Lambda event data for the handler.
+ * @param context AWS Lambda runtime information.
+ */
+exports.tweetTime = function(event, context) {
+    let currentdate = new Date();
+    tweet(currentdate.toLocaleTimeString(), context.succeed, context.fail);
+};

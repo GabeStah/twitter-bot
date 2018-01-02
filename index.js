873f9c5e5b5b9f19b765a9f05a421356e14efad9
Gabe Wyatt <gabe@gabewyatt.com>
2018-01-01 16:21:22 -0800

    Added and integrated Airbrake-JS.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // index.js
+const airbrake_credentials = require('./airbrake-credentials');
+const AirbrakeClient = require('airbrake-js');
 const FeedParser = require('feedparser');
 const request = require('request');
 const twitter_credentials = require('./twitter-api-credentials');
 const Twitter = require('twitter');
+
+// Use exported secret credentials.
+let airbrake = new AirbrakeClient(airbrake_credentials);
 
 let feedparser = new FeedParser();
 let feed = request('https://airbrake.io/blog/feed/atom');
@@ -18,7 +23,7 @@ let articles = [];
  */
 feed.on('response', function (response) {
     if (response.statusCode !== 200) {
-        this.emit('error', new Error('Bad status code'));
+        this.emit('error', new Error('Bad status code.'));
     } else {
         // Pipes request to feedparser for processing.
         this.pipe(feedparser);
@@ -48,26 +53,51 @@ feedparser.on('readable', function () {
 });
 
 /**
+ * Tweet the passed string message.
+ *
+ * @param message String to be tweeted.
+ */
+function tweet(message) {
+    if (message === null || message === '') return;
+    twitter.post(
+        'statuses/update',
+        {
+            status: message
+        },
+        tweetCallback
+    );
+}
+
+/**
  * Tweet the passed Article object.
  *
  * @param article Article to be tweeted.
  */
 function tweetArticle(article) {
-    if (article == null) return;
+    if (article === null) return;
     twitter.post(
         'statuses/update',
         {
             status: `${article.title} ${article.link}`
         },
-        function(error, tweet, response) {
-            if(error) {
-                console.log(error);
-                throw error;
-            }
-            console.log('---- TWEETED ARTICLE ----');
-            console.log(tweet);
-        }
+        tweetCallback
     );
+}
+
+/**
+ * Callback for tweet attempts.
+ *
+ * @param error Caught error.
+ * @param tweet Tweet.
+ * @param response Response.
+ */
+function tweetCallback(error, tweet, response) {
+    if(error) {
+        console.log(error);
+        throw new Error(error[0].message);
+    }
+    console.log('---- TWEETED ----');
+    console.log(tweet);
 }
 
 /**
